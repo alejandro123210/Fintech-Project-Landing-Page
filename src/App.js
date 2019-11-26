481d06a23cb254ed9ed8f53767842f6138ad02f3
Alejandro Gonzalez <myname123210@gmail.com>
2019-11-26 15:53:22 -0500

    first commit!
    Made a skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { reverse } from 'dns';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{fontSize: 80, paddingLeft: 30, color: 'white'}}>
+            Peerfund
+        </p> 
+        <p style={{fontSize: 50, color: 'white'}}> Stop letting banks control your life.<br/> Join the revolution.</p>
+        <p style={{fontSize: 30, paddingBottom: 30, color: 'white', paddingTop: '10%'}}>
+            Find out more!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="App-what-we-do">
+        <p style={{fontSize: 40, paddingTop: 20, color: 'black', paddingLeft: 30}}>
+          What we do:
+        </p>
+        <p>
+          Peerfund is the first ever personal bond marketplace.
+        </p>
+        <div className="App-explanation-boxes-container">
+          <div className="App-explanation-box">
+            <p style={{paddingTop: '60%'}}> A borrower asks for a loan with their own given terms</p>
+          </div>
+          <div className="App-explanation-box">
+            <p style={{paddingTop: '60%'}}> Lenders choose to buy portions of that loan as bonds</p>
+          </div>
+          <div className="App-explanation-box">
+            <p style={{paddingTop: '60%'}}> Lenders are then able to trade the bonds, or wait to be paid back</p>
+          </div>
+        </div>
+        <div className="App-our-goal">
+          <p style={{fontSize: 40, color: 'white'}}> Why?</p>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
